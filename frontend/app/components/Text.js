2a5ea0a7dc4951c2ac11a22ae05cf871dd4f3026
ayush-rathore <aayushh79@gmail.com>
2021-11-08 23:09:36 +0530

    Add font

--- frontend/app/components/Text.js
+++ frontend/app/components/Text.js
@@ -2,22 +2,14 @@
 
 import React from "react";
 import { Text, StyleSheet } from "react-native";
-import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
-import { AppLoading } from "expo-app-loading";
 
 function AppText({ children, style }) {
-	let [fontsLoaded] = useFonts({ Poppins_400Regular });
-
-	if (!fontsLoaded) {
-		return <AppLoading />;
-	} else {
-		return <Text style={[styles.text, style]}>{children}</Text>;
-	}
+	return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
 	text: {
-		fontFamily: "Poppins_400Regular",
+		fontFamily: "Poppins-Regular",
 	},
 });
 
