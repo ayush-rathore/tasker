f6ceb169985e52c3373874ada02d6427bafa34c9
ayush-rathore <aayushh79@gmail.com>
2021-11-11 19:37:23 +0530

    Update text component

---
+++ frontend/app/components/AppText.js
@@ -0,0 +1,19 @@
+// Text component
+
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import colors from "../config/colors";
+
+function AppText({ children, style }) {
+	return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 20,
+		fontFamily: "Roboto",
+		color: colors.dark,
+	},
+});
+
+export default AppText;
