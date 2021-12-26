054ec4289b1100a3dc740efab3530b169bc16c08
ayush-rathore <aayushh79@gmail.com>
2021-12-26 17:56:01 +0530

    Add "/" route

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./config/database").connect();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -10,5 +11,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.get("/", (req, res) => {
+	return res.status(200).send("Hello World!");
+});
 
 module.exports = app;
