865a489934262dbe07ba160bba4264ae0a6f747f
ayush-rathore <aayushh79@gmail.com>
2021-12-27 13:56:48 +0530

    Use Navigation

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import React from "react";
-import Welcome from "./app/screens/Welcome";
-import LoginScreen from "./app/screens/Login";
-import RegisterScreen from "./app/screens/Register";
+import React, { useState } from "react";
+import "react-native-gesture-handler";
 
-export default function App() {
-	return <RegisterScreen />;
-}
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import NavigationTheme from "./app/navigation/NavigationTheme";
+
+import UserContext from "./app/context/context";
+
+const App = () => {
+	const [user, setUser] = useState();
+	const [tasks, setTasks] = useState([]);
+
+	return (
+		<UserContext.Provider value={{ user, setUser, tasks, setTasks }}>
+			<NavigationContainer theme={NavigationTheme}>
+				{user ? <AppNavigator /> : <AuthNavigator />}
+			</NavigationContainer>
+		</UserContext.Provider>
+	);
+};
+
+export default App;
