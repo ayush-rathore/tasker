750dc42957bde87d15a69e3efa806f1d61a920c6
ayush-rathore <aayushh79@gmail.com>
2021-12-28 16:42:27 +0530

    Use task instead of app

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "react-native-gesture-handler";
 
 import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./app/navigation/AppNavigator";
+import TaskNavigator from "./app/navigation/TaskNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import NavigationTheme from "./app/navigation/NavigationTheme";
 
@@ -15,7 +15,7 @@ const App = () => {
 	return (
 		<UserContext.Provider value={{ user, setUser, tasks, setTasks }}>
 			<NavigationContainer theme={NavigationTheme}>
-				{user ? <AppNavigator /> : <AuthNavigator />}
+				{user ? <TaskNavigator /> : <AuthNavigator />}
 			</NavigationContainer>
 		</UserContext.Provider>
 	);
