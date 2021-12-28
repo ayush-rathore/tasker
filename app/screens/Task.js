f0e82588487a6df54624e7ce6498a22052c8f276
ayush-rathore <aayushh79@gmail.com>
2021-12-28 20:43:38 +0530

    Update Task screen

--- app/screens/Task.js
+++ app/screens/Task.js
@@ -6,16 +6,22 @@ import { FlatList, StyleSheet } from "react-native";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
-import ActionButton from "react-native-action-button";
 
-import client from "../api/client";
+import useApi from "../hooks/useApi";
+import task from "../api/task";
 import User from "../context/User";
+import client from "../api/client";
+import Spinner from "react-native-loading-spinner-overlay";
+import AddButton from "../components/AddButton";
+import NoTask from "../components/NoTask";
 
 const TaskScreen = ({ navigation }) => {
-	const { user, tasks, addTask } = User();
+	const { user, tasks, addTask, logout } = User();
 
 	const getTasks = async () => {
-		const response = await client.get(`/task/get/${user._id}`);
+		const userID = user._id;
+		// const response = await taskApi.request(userID);
+		const response = await client.get(`/task/get/${userID}`);
 		addTask(response.data);
 	};
 
@@ -25,28 +31,30 @@ const TaskScreen = ({ navigation }) => {
 
 	return (
 		<Screen style={styles.screen}>
-			<FlatList
-				data={tasks}
-				keyExtractor={(item) => item._id}
-				renderItem={({ item }) => (
-					<Card
-						name={item.name}
-						description={item.description}
-						taskID={item._id}
-					/>
-				)}
-			/>
-			<ActionButton
-				buttonColor="#1E88E5"
-				onPress={() => navigation.navigate("AddTask")}
-			/>
+			{/* <Spinner visible={loading} color="#1E88E5" /> */}
+			{tasks.length == 0 ? (
+				<NoTask />
+			) : (
+				<FlatList
+					data={tasks}
+					keyExtractor={(item) => item._id}
+					renderItem={({ item }) => (
+						<Card
+							name={item.name}
+							description={item.description}
+							taskID={item._id}
+						/>
+					)}
+				/>
+			)}
+			<AddButton onPress={() => navigation.navigate("AddTask")} />
 		</Screen>
 	);
 };
 
 const styles = StyleSheet.create({
 	screen: {
-		backgroundColor: colors.primary,
+		backgroundColor: colors.white,
 	},
 });
 
