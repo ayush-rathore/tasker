18b9de39b2eea6f26b0dfa234ac5f92afb9f20fb
ayush-rathore <aayushh79@gmail.com>
2021-12-28 16:41:10 +0530

    Add task navigator

--- app/navigation/TaskNavigator.js
+++ app/navigation/TaskNavigator.js
@@ -2,12 +2,24 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import TaskScreen from "../screens/Task";
+import AddTask from "../screens/AddTask";
 
 const Stack = createStackNavigator();
 
 const TaskNavigator = () => (
 	<Stack.Navigator>
-		<Stack.Screen name="Task" component={TaskScreen}></Stack.Screen>
+		<Stack.Screen
+			name="Task"
+			component={TaskScreen}
+			options={{
+				title: "My tasks",
+			}}
+		></Stack.Screen>
+		<Stack.Screen
+			name="AddTask"
+			component={AddTask}
+			options={{ title: "Add Task" }}
+		></Stack.Screen>
 	</Stack.Navigator>
 );
 
