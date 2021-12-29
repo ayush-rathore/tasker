2b9e63680c8e2315f698a0afa6d181b468a4d6bc
ayush-rathore <aayushh79@gmail.com>
2021-12-30 00:21:21 +0530

    Remove expo-constants

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -2,16 +2,9 @@
 
 import React from "react";
 import { View } from "react-native";
-import Constants from "expo-constants";
 
 function Screen({ children, style }) {
-	return (
-		<View
-			style={[{ paddingTop: Constants.statusBarHeight, flex: 1 }, style]}
-		>
-			{children}
-		</View>
-	);
+	return <View style={[{ paddingTop: 10, flex: 1 }, style]}>{children}</View>;
 }
 
 export default Screen;
