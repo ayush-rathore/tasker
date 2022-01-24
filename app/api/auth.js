1ca829b28665a48d4317edafbcdb4795d2494c66
Ayush Rathore <aayushh79@gmail.com>
2022-01-24 13:34:47 +0530

    Re-structure the app

--- app/api/auth.js
+++
@@ -1,7 +0,0 @@
-import client from "./client";
-
-const login = (userInfo) => client.post("/user/login", userInfo);
-
-const register = (userInfo) => client.post("/user/register", userInfo);
-
-export default { login, register };
--- app/api/task.js
+++
@@ -1,9 +0,0 @@
-import client from "./client";
-
-const get = (userID) => client.get(`/task/get/${userID}`);
-
-const add = (taskInfo) => client.post("/task/add", taskInfo);
-
-const remove = (taskID) => client.post(`/task/remove/${taskID}`);
-
-export default { get, add, remove };
